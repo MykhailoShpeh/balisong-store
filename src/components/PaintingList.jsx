4a2e6e8bc578e00bbcebce57513fd66ed5c86749
Michael Shpeh <mykhailoshpeh@gmail.com>
2026-02-08 13:40:18 +0200

    final step

---
+++ src/components/PaintingList.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+import Painting from './Painting';
+
+
+function PaintingsList({ items }) {
+    return (
+        <ul>
+            {items.map(item =>
+                <li key={item.id}>
+                    <Painting
+                        url={item.url}
+                        title={item.title}
+                        author={item.author.tag}
+                        profileUrl={item.author.url}
+                        price={item.price}
+                        quantity={item.quantity}
+                    />
+                </li>
+            )}
+        </ul>
+    );
+};
+
+
+PaintingsList.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+        }),
+    ),
+};
+
+
+export default PaintingsList;
